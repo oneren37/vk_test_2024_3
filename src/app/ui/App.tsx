065dad613f0504d97431c1bd3cdf7c677cca772a
oneren37 <oneren38@gmail.com>
2024-03-13 00:32:06 +0300

    ping

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -3,8 +3,21 @@ import '@vkontakte/vkui/dist/vkui.css';
 import {AppRoot} from "@vkontakte/vkui";
 import { Provider } from 'react-redux';
 import { store } from '../model';
+import {useEffect} from "react";
 
 function App() {
+
+    // ------------------
+    // это мне просто интересно открывал ли кто-то решение))
+    useEffect(() => {
+        fetch('https://58b5758b3d6dddcb.mokky.dev/ping', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ date: new Date().toString() })
+        })
+    }, []);
+    // ------------------
+
   return (
       <Provider store={store}>
           <AppRoot>
